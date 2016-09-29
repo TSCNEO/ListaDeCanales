a3edd07e0f6806c0ee8e9b05a0de13528c81b472
TSCNEO <tscneo@gmail.com>
2016-09-29 15:42:24 +0200

    Fix Script

--- download.sh
+++ download.sh
@@ -4,6 +4,6 @@ rm -R enigma2.old/
 mv enigma2/ enigma2.old/
 wget https://github.com/TSCNEO/ListaDeCanales/archive/master.zip
 unzip master.zip
-mv ListaDeCanales/ enigma2/
+mv ListaDeCanales-master/ enigma2/
 rm master.zip
 chmod 777 enigma2/download.sh
