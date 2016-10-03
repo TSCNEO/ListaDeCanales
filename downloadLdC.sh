06334cdd24b70df2e42fb3ec1b3e7d59e55ec17b
TSCNEO <tscneo@gmail.com>
2016-10-03 11:56:51 +0200

    Fixed Script

--- downloadLdC.sh
+++ downloadLdC.sh
@@ -8,3 +8,4 @@ mv ListaDeCanales-master/ enigma2/
 rm master.zip
 chmod 777 /etc/enigma2/downloadLdC.sh
 mv /etc/enigma2/downloadLdC.sh /usr/script/downloadLdC.sh
+wget -qO - http://127.0.0.1/web/servicelistreload?mode=0
